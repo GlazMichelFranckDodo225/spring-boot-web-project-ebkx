5023341199eaaa82fbeeb9a0cf68bde2d949e289
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-21 07:07:07 +0100

    Project Initialization

---
+++ src/test/java/com/dgmf/SpringBootWebProjectEbkxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebProjectEbkxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
